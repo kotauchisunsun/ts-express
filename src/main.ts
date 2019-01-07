d9f0432d1fa6e6f853931ddc17e0245623c74410
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2019-01-08 02:23:01 +0900

    main.tsを作成

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import * as express from "express";
+import { makeApp } from "./makeApp";
+import { InMemoryUserRepository } from "./repository/user/InMemoryUserRepository";
+import { CreateUserUseCase } from "./domain/use_case/CreateUserUseCase";
+import { UserService } from "./domain/UserService";
+import { ReadUserUseCase } from "./domain/use_case/ReadUserUseCase";
+import { UpdateUserUseCase } from "./domain/use_case/UpdateUserUseCase";
+import { DeleteUserUseCase } from "./domain/use_case/DeleteUserUseCase";
 
-const app: express.Application = express();
+const repository = new InMemoryUserRepository();
+const service = new UserService(repository);
 
-app.get(
-  "/",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.write("Hello");
-    res.end();
-  }
+const createUserUseCase = new CreateUserUseCase(repository, service);
+const readUserUseCase = new ReadUserUseCase(repository);
+const updateUserUseCase = new UpdateUserUseCase(repository, service);
+const deleteUserUseCase = new DeleteUserUseCase(repository);
+
+const app = makeApp(
+  createUserUseCase,
+  readUserUseCase,
+  updateUserUseCase,
+  deleteUserUseCase
 );
 
 app.listen(3000);
